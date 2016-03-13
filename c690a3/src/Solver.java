a822eaa803c16315ca0950865c53c8630697306c
escheung <escheung@c690>
2016-03-13 14:09:04 -0600

    First commit.

---
+++ c690a3/src/Solver.java
@@ -0,0 +1,19 @@
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.vocabulary.*;
+
+
+
+public class Solver {
+
+	
+	private static final String SOURCE_RDF = "cmput690_a3.ttl";
+	
+	public static void main(String[] args) {
+
+		Model model = RDFDataMgr.loadModel(SOURCE_RDF);
+		
+		
+	}
+
+}
